60b06eb1007bda446fad2ebc3098fa0e7e01ac13
himanshuabd17 <himanshu12061998@gmail.com>
2023-03-25 13:47:19 +0530

    fourth job

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const shortid=require("shortid");
+// const shortid=require("shortid");
 
-//generating the ids using the shortid package.
-console.log(shortid.generate());
+// //generating the ids using the shortid package.
+// console.log(shortid.generate());
 
-console.log("hey there");
+console.log('hey there');
